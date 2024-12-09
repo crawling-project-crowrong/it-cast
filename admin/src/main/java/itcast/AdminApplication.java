139d577df8b8e65c6b89a87df82064cee265287b
kimjihye <hamiwood@naver.com>
2024-12-09 15:39:58 +0900

    chore : AdminApplication.java 설정 변경

--- admin/src/main/java/itcast/AdminApplication.java
+++ admin/src/main/java/itcast/AdminApplication.java
@@ -2,8 +2,12 @@ package itcast;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
+@EntityScan(basePackages = {"domain"})
+@EnableJpaRepositories(basePackages = {"itcast.repository"})
+@SpringBootApplication(scanBasePackages = {"itcast", "domain"})
 public class AdminApplication {
     public static void main(String[] args) {
         SpringApplication.run(AdminApplication.class, args);
