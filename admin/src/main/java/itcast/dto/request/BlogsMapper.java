ca7048c57700a83d71a0e4d33b88532ed47f14c0
kimjihye <hamiwood@naver.com>
2024-12-10 12:02:27 +0900

    feat: blog entity 수정, dto 생성

---
+++ admin/src/main/java/itcast/dto/request/BlogsMapper.java
@@ -0,0 +1,23 @@
+package itcast.dto.request;
+
+import itcast.domain.blog.Blog;
+import org.springframework.stereotype.Component;
+
+@Component
+public class BlogsMapper {
+    public static Blog toEntity(AdminBlogRequest adminBlogRequest) {
+        return Blog.builder()
+                .platform(adminBlogRequest.getPlatform())
+                .title(adminBlogRequest.getTitle())
+                .content(adminBlogRequest.getContent())
+                .originalContent(adminBlogRequest.getOriginalContent())
+                .interest(adminBlogRequest.getInterest())
+                .publishedAt(adminBlogRequest.getPublishedAt())
+                .rating(adminBlogRequest.getRating())
+                .link(adminBlogRequest.getLink())
+                .thumbnail(adminBlogRequest.getThumbnail())
+                .status(adminBlogRequest.getStatus())
+                .sendAt(adminBlogRequest.getSendAt())
+                .build();
+    }
+}
