77ff22d66462a7c5676d554b8294f786a1236dcd
kimjihye <hamiwood@naver.com>
2024-12-10 19:11:28 +0900

    feat: 관리자 뉴스 조회기능 controller, service, Repository 생성, Paging처리

--- admin/src/main/java/itcast/repository/NewsRepository.java
+++ admin/src/main/java/itcast/repository/NewsRepository.java
@@ -1,9 +1,19 @@
 package itcast.repository;
 
 import itcast.domain.news.News;
+import itcast.domain.news.enums.NewsStatus;
+import itcast.dto.response.AdminNewsResponse;
+import jakarta.persistence.TypedQuery;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface NewsRepository extends JpaRepository<News, Long> {
+
+    @Query("SELECT n FROM News n WHERE n.status = :status ORDER BY n.sendAt DESC ")
+    Page<AdminNewsResponse> findAllByStatusOrderBySendAtDesc(@Param("status") NewsStatus status, Pageable pageable);
 }
