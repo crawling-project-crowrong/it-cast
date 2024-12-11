74db39a992046ab4f8b21571fc3abb1432d03db5
seoyeong-4811 <tjdud9213@naver.com>
2024-12-11 13:23:41 +0900

    [#9] feat: 회원정보 수정 기능 완료

--- b2c/src/main/java/itcast/user/application/UserService.java
+++ b2c/src/main/java/itcast/user/application/UserService.java
@@ -6,7 +6,9 @@ import itcast.domain.user.User;
 import itcast.domain.user.enums.ArticleType;
 import itcast.domain.user.enums.Interest;
 import itcast.user.dto.request.ProfileCreateRequest;
+import itcast.user.dto.request.ProfileUpdateRequest;
 import itcast.user.dto.response.ProfileCreateResponse;
+import itcast.user.dto.response.ProfileUpdateResponse;
 import itcast.user.exception.EmailAlreadyExistsException;
 import itcast.user.exception.NicknameAlreadyExistsException;
 import itcast.user.exception.UserNotFoundException;
@@ -40,5 +42,28 @@ public class UserService {
 		User updatedUser = request.toEntity(existingUser);
 		User savedUser = userRepository.save(updatedUser);
 		return ProfileCreateResponse.fromEntity(savedUser);
+	}
+
+	public ProfileUpdateResponse updateProfile(ProfileUpdateRequest request, Long id) {
+		User existingUser = userRepository.findById(id)
+			.orElseThrow(() -> new UserNotFoundException("사용자를 찾을 수 없습니다."));
+		if (userRepository.existsByNickname(request.nickname())) {
+			throw new NicknameAlreadyExistsException("이미 사용 중인 닉네임입니다.");
+		}
+		if (userRepository.existsByEmail(request.email())) {
+			throw new EmailAlreadyExistsException("이미 사용 중인 이메일입니다.");
+		}
+		if (ArticleType.NEWS.equals(request.articleType())) {
+			request = new ProfileUpdateRequest(
+				request.nickname(),
+				request.articleType(),
+				Interest.NEWS,
+				request.sendingType(),
+				request.email()
+			);
+		}
+		User updatedUser = request.toEntity(existingUser);
+		User savedUser = userRepository.save(updatedUser);
+		return ProfileUpdateResponse.fromEntity(savedUser);
 	}
 }
