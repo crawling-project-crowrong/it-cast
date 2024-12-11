f38ee2c521462c378706f9808e60ab052ea4f034
kimjihye <hamiwood@naver.com>
2024-12-11 09:53:54 +0900

    refactor: 응답형식을 ResponseTemplate으로 변경

--- admin/src/main/java/itcast/controller/AdminController.java
+++ admin/src/main/java/itcast/controller/AdminController.java
@@ -3,10 +3,9 @@ package itcast.controller;
 import itcast.application.AdminService;
 import itcast.dto.request.AdminNewsRequest;
 import itcast.dto.response.AdminNewsResponse;
-import itcast.dto.response.ResponseBodyDto;
+import itcast.dto.response.ResponseTemplate;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -17,9 +16,9 @@ public class AdminController {
     private final AdminService adminService;
 
     @PostMapping("/news")
-    public ResponseEntity<ResponseBodyDto<AdminNewsResponse>> createNews(@RequestParam Long userId, @RequestBody AdminNewsRequest adminNewsRequest) {
-        return new ResponseEntity<>(
-                ResponseBodyDto.success("관리자 뉴스 생성 성공",
-                        adminService.createNews(userId, adminNewsRequest)), HttpStatus.CREATED);
+    public ResponseTemplate<AdminNewsResponse> createNews(@RequestParam Long userId, @RequestBody AdminNewsRequest adminNewsRequest) {
+        AdminNewsResponse response = adminService.createNews(userId, adminNewsRequest);
+
+        return new ResponseTemplate<>(HttpStatus.CREATED,"관리자 뉴스 생성 성공", response);
     }
 }
