61d99f90cea4081207e09250c60f205719554f25
kimjihye <hamiwood@naver.com>
2024-12-11 12:28:24 +0900

    refactor: 명시적으로 사용한 @repository 제거

--- admin/src/main/java/itcast/repository/AdminRepository.java
+++ admin/src/main/java/itcast/repository/AdminRepository.java
@@ -2,9 +2,7 @@ package itcast.repository;
 
 import itcast.domain.admin.Admin;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface AdminRepository extends JpaRepository<Admin, Long> {
     boolean existsByEmail(String email);
 }
--- admin/src/main/java/itcast/repository/BlogRepository.java
+++ admin/src/main/java/itcast/repository/BlogRepository.java
@@ -2,8 +2,6 @@ package itcast.repository;
 
 import itcast.domain.blog.Blog;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface BlogRepository extends JpaRepository<Blog, Long> {
 }
