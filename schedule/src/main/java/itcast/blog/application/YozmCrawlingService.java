cea31ea2223754f14d89194bee04df102c276537
shinheekim <shinhee1241@gmail.com>
2024-12-13 16:26:41 +0900

    [#14] refactor: service단으로 로직 옮기기

--- schedule/src/main/java/itcast/blog/application/YozmCrawlingService.java
+++ schedule/src/main/java/itcast/blog/application/YozmCrawlingService.java
@@ -1,11 +1,15 @@
 package itcast.blog.application;
 
+import itcast.blog.controller.YozmJsoupCrawler;
+import itcast.blog.repository.BlogRepository;
 import itcast.domain.blog.Blog;
 import itcast.domain.blog.enums.BlogStatus;
 import itcast.domain.blog.enums.Platform;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.jsoup.nodes.Document;
 import org.jsoup.select.Elements;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
@@ -14,14 +18,16 @@ import java.util.List;
 import java.util.Objects;
 import java.util.stream.IntStream;
 
-import static itcast.blog.controller.YozmJsoupCrawler.getHtmlDocument;
-
 @Service
 @Slf4j
+@RequiredArgsConstructor
 public class YozmCrawlingService {
 
+    private final static int MAX_PAGE = 6;
     private static final String BASE_URL = "https://yozm.wishket.com/magazine/list/develop/?sort=new&page=";
     private static final String SORTED_URL = "&sort=new&q=";
+    private final YozmJsoupCrawler yozmJsoupCrawler;
+    private final BlogRepository blogRepository;
 
     public List<Blog> crawlBlogs(int maxPage) {
         List<Blog> blogs = IntStream.range(1, maxPage)
@@ -52,9 +58,19 @@ public class YozmCrawlingService {
         return blogs;
     }
 
+    @Scheduled(cron = "${crawler.yozm.cron}")
+    public void yozmCrawling() {
+        log.info("블로그 크롤링 시작 ...");
+
+        List<Blog> blogs = crawlBlogs(MAX_PAGE);
+        blogRepository.saveAll(blogs);
+
+        log.info("블로그 크롤링 및 저장 완료!");
+    }
+
     private Document getHtmlDocumentOrNull(String url) {
         try {
-            return getHtmlDocument(url);
+            return yozmJsoupCrawler.getHtmlDocument(url);
         } catch (IOException e) {
             log.error("Document Parse Error", e);
             return null;
