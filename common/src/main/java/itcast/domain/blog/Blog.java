801e5a181746eb675716f9fc222badd014a13803
kimjihye <hamiwood@naver.com>
2024-12-16 03:01:06 +0900

    fix: 생성 시 기본 빌더 값이 넘어가는 오류 해결

--- common/src/main/java/itcast/domain/blog/Blog.java
+++ common/src/main/java/itcast/domain/blog/Blog.java
@@ -78,9 +78,17 @@ public class Blog extends BaseEntity {
         this.status = status;
     }
 
-    @Builder(builderMethodName = "adminBuilder")
-    public Blog(Platform platform, String title, String content, String originalContent, Interest interest,
-                LocalDateTime publishedAt, int rating, String link, String thumbnail, BlogStatus status, LocalDateTime sendAt) {
+    @Builder(builderClassName = "adminBuilder", builderMethodName = "adminBuilder")
+    public Blog(Platform platform,
+                String title,
+                String content,
+                String originalContent,
+                Interest interest,
+                LocalDateTime publishedAt,
+                int rating, String link,
+                String thumbnail,
+                BlogStatus status,
+                LocalDateTime sendAt) {
         this.platform = platform;
         this.title = title;
         this.content = content;
