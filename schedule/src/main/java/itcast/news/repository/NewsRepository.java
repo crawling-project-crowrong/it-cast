5afdce2c16103a807e3f48f1b6bbdeb246d8d65f
jubseok <junseok708@gmail.com>
2024-12-16 10:14:03 +0900

    [#26] fix : comment 수정

--- schedule/src/main/java/itcast/news/repository/NewsRepository.java
+++ schedule/src/main/java/itcast/news/repository/NewsRepository.java
@@ -10,10 +10,12 @@ import java.time.LocalDate;
 import java.util.List;
 
 public interface NewsRepository extends JpaRepository<News, Long> {
-   @Query("select n.link from News n")
+    @Query("select n.link from News n")
     List<String> findAllLinks();
+
     @Query("select n from News n where function('DATE',n.createdAt) = :yesterday ")
     List<News> findAllByCreatedAt(@Param("yesterday") LocalDate yesterday);
+
     @Modifying
     @Query("DELETE FROM News n WHERE n.createdAt <= CURRENT_DATE - 6 MONTH")
     void deleteOldNews();
