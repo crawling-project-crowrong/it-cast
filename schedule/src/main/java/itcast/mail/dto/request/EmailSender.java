82d14371a6e3382af2583a8f27ae9ed0434e0b63
yuseonjun <seonjun0906@gmail.com>
2024-12-17 19:33:55 +0900

    [#63] refactor: 썸네일 이미지 나오게 변경

--- schedule/src/main/java/itcast/mail/dto/request/EmailSender.java
+++ schedule/src/main/java/itcast/mail/dto/request/EmailSender.java
@@ -5,15 +5,10 @@ import com.amazonaws.services.simpleemail.model.Content;
 import com.amazonaws.services.simpleemail.model.Destination;
 import com.amazonaws.services.simpleemail.model.Message;
 import com.amazonaws.services.simpleemail.model.SendEmailRequest;
-import java.io.IOException;
-import java.util.Base64;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Component;
-import org.springframework.util.StreamUtils;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.Context;
 
@@ -56,19 +51,6 @@ public class EmailSender {
         context.setVariable("subject", MAIL_SUBJECT);
         context.setVariable("contents", request.contents());
 
-        try {
-            final String base64Image = getBase64EncodedImage("/static/images/logo.png");
-            context.setVariable("logoImage", base64Image);
-        } catch (IOException e) {
-            throw new RuntimeException(e.getMessage(), e);
-        }
-
         return templateEngine.process("email-template", context);
-    }
-
-    private String getBase64EncodedImage(final String imagePath) throws IOException {
-        final Resource resource = new ClassPathResource(imagePath);
-        byte[] bytes = StreamUtils.copyToByteArray(resource.getInputStream());
-        return Base64.getEncoder().encodeToString(bytes);
     }
 }
