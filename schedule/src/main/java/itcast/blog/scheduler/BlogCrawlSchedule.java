da7c885d6b252cc7b70e57c937be3617f297e3f4
shinheekim <shinhee1241@gmail.com>
2024-12-17 19:34:29 +0900

    [#16] chore: 줄 띄우기

--- schedule/src/main/java/itcast/blog/scheduler/BlogCrawlSchedule.java
+++ schedule/src/main/java/itcast/blog/scheduler/BlogCrawlSchedule.java
@@ -7,7 +7,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
-@Slf4j
+@Slf4j(topic = "블로그 크롤링 스케쥴")
 @Component
 @RequiredArgsConstructor
 public class BlogCrawlSchedule {
@@ -19,14 +19,18 @@ public class BlogCrawlSchedule {
     @Scheduled(cron = "${scheduler.velog.crawling}")
     public void velogCrawling() {
         log.info("Velog Crawling Start ...");
+
         velogCrawlingService.crawlBlogs();
+
         log.info("Velog Crawling & Save!");
     }
 
     @Scheduled(cron = "${scheduler.yozm.crawling}")
     public void yozmCrawling() {
         log.info("Yozm Crawling Start ...");
+
         yozmCrawlingService.crawlBlogs();
+
         log.info("Yozm Crawling & Save!");
     }
 }
