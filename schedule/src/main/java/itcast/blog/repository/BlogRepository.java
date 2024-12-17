7ba9109e946475d067b3e692dec3994c894d038a
shinheekim <shinhee1241@gmail.com>
2024-12-17 19:36:00 +0900

    [#16] feat: 블로그 선택 스케쥴 완료

--- schedule/src/main/java/itcast/blog/repository/BlogRepository.java
+++ schedule/src/main/java/itcast/blog/repository/BlogRepository.java
@@ -1,13 +1,28 @@
 package itcast.blog.repository;
 
 import itcast.domain.blog.Blog;
-
+import itcast.domain.blog.enums.Platform;
+import itcast.domain.user.enums.Interest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.List;
 
 public interface BlogRepository extends JpaRepository<Blog, Long> {
     @Query("SELECT b.link FROM Blog b")
     List<String> findAllLinks();
+
+    @Query("""
+            SELECT b FROM Blog b
+            WHERE b.sendAt IS NULL
+            AND b.publishedAt >= :recentDate
+            AND b.rating >= :minRating
+            AND b.platform = :platform
+            AND b.interest = :interset
+            ORDER BY b.rating, b.publishedAt DESC
+            """)
+    List<Blog> findByBlogForSelection(Platform platform, Interest interest, LocalDate recentDate, int minRating, Pageable pageable);
 }
