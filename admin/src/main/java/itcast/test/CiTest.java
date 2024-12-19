3a2e55c1c3d1a1cc8a19ab6eb6f0ea0fef04d9b4
kimjihye <hamiwood@naver.com>
2024-12-19 19:43:24 +0900

    feat: Ci 실패 테스트(컴파일 에러)

---
+++ admin/src/main/java/itcast/test/CiTest.java
@@ -0,0 +1,8 @@
+package itcast.test;
+
+public class CiTest {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        int error //세미콜론 누락으로 컴파일 에러 유도
+    }
+}
