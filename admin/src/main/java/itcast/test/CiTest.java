8d59b133a2b29744b7c31c2a834408ada66c5743
kimjihye <hamiwood@naver.com>
2024-12-19 20:39:45 +0900

    test: Ci 성공 테스트

--- admin/src/main/java/itcast/test/CiTest.java
+++ admin/src/main/java/itcast/test/CiTest.java
@@ -3,6 +3,6 @@ package itcast.test;
 public class CiTest {
     public static void main(String[] args) {
         System.out.println("Hello World");
-        int error //세미콜론 누락으로 컴파일 에러 유도
+        int error; //컴파일 에러 수정(Ci 성공테스트)
     }
 }
