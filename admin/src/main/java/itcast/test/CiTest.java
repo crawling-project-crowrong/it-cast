34bbef705a38f2179199e1a6b6136a65eb5091e2
kimjihye <hamiwood@naver.com>
2024-12-19 20:55:47 +0900

    fix: build-and-deploy가 보이지 않는 문제

--- admin/src/main/java/itcast/test/CiTest.java
+++ admin/src/main/java/itcast/test/CiTest.java
@@ -3,6 +3,6 @@ package itcast.test;
 public class CiTest {
     public static void main(String[] args) {
         System.out.println("Hello World");
-        int error //컴파일 에러 발생
+        int error
     }
 }
