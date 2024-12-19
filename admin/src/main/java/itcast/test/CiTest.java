277a42e5c99623252cada29dfb30e622c5d5036d
kimjihye <hamiwood@naver.com>
2024-12-19 20:46:59 +0900

    test: Ci 실패 테스트(재)

--- admin/src/main/java/itcast/test/CiTest.java
+++ admin/src/main/java/itcast/test/CiTest.java
@@ -3,6 +3,6 @@ package itcast.test;
 public class CiTest {
     public static void main(String[] args) {
         System.out.println("Hello World");
-        int error; //컴파일 에러 수정(Ci 성공테스트)
+        int error //컴파일 에러 발생
     }
 }
