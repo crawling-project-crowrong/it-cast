5526388c0eb2da012058a687f2fbb4cd2a2e7e51
yuseonjun <seonjun0906@gmail.com>
2024-12-19 18:43:33 +0900

    [#75] feat: ErrorCode 추가

--- common/src/main/java/itcast/exception/ErrorCodes.java
+++ common/src/main/java/itcast/exception/ErrorCodes.java
@@ -1,11 +1,16 @@
 package itcast.exception;
 
+import lombok.Getter;
 import org.springframework.http.HttpStatus;
 
+@Getter
 public enum ErrorCodes {
 
     BLOG_NOT_FOUND("블로그가 유효하지 않습니다.", 1001L, HttpStatus.NOT_FOUND),
     NEWS_NOT_FOUND("뉴스가 유효하지 않습니다.", 2001L, HttpStatus.NOT_FOUND),
+
+    USER_NOT_FOUND("유저가 유효하지 않습니다.", 3001L, HttpStatus.NOT_FOUND),
+    INVALID_USER("접근할 수 없는 유저입니다.", 3002L, HttpStatus.FORBIDDEN),
 
     BAD_REQUEST("BAD_REQUEST", 9404L, HttpStatus.BAD_REQUEST),
     BAD_REQUEST_JSON_PARSE_ERROR("[BAD_REQUEST] JSON_PARSE_ERROR - 올바른 JSON 형식이 아님", 9405L, HttpStatus.BAD_REQUEST),
