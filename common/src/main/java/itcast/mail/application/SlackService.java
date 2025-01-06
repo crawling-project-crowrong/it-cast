01a2c8825311fec67651d1a5c6dde37bec0bf1a8
jubseok <junseok708@gmail.com>
2025-01-06 12:56:55 +0900

    [#103] fix: 밸류 수정 실패시 확인 로직 생성

--- common/src/main/java/itcast/mail/application/SlackService.java
+++ common/src/main/java/itcast/mail/application/SlackService.java
@@ -3,8 +3,10 @@ package itcast.mail.application;
 import com.slack.api.Slack;
 import com.slack.api.methods.MethodsClient;
 import com.slack.api.methods.request.chat.ChatPostMessageRequest;
+import com.slack.api.methods.response.chat.ChatPostMessageResponse;
 import com.slack.api.model.block.composition.TextObject;
 import itcast.exception.ItCastApplicationException;
+import itcast.mail.dto.request.SlackRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -22,18 +24,20 @@ import static itcast.exception.ErrorCodes.SLACK_PARSE_ERROR;
 @RequiredArgsConstructor
 public class SlackService {
 
-    private static final String TOKEN = "token";
-    private static final String CHANNEL = "channel";
+    @Value("${slack.token}")
+    private String token;
+    @Value("${slack.channel.monitor}")
+    private String channel;
 
-    public void postInquiry(String receiver) {
+    public void postInquiry(SlackRequestDto slackRequestDto) {
         try {
             List<TextObject> textObjects = new ArrayList<>();
             textObjects.add(markdownText("*재 발송 실패 날짜:*\n" + LocalDateTime.now()));
-            textObjects.add(markdownText("*재 시도 실패 메일:*\n" +receiver));
+            textObjects.add(markdownText("*재 시도 실패 메일:*\n" + slackRequestDto.receiver()));
 
-            MethodsClient methods = Slack.getInstance().methods(TOKEN);
+            MethodsClient methods = Slack.getInstance().methods(token);
             ChatPostMessageRequest request = ChatPostMessageRequest.builder()
-                    .channel(CHANNEL)
+                    .channel(channel)
                     .blocks(asBlocks(
                             header(header ->
                                     header.text(plainText("메일 재 발송에 실패 했습니다!!!"))),
@@ -42,8 +46,13 @@ public class SlackService {
                                     section.fields(textObjects)
                             ))).build();
 
-            methods.chatPostMessage(request);
+            ChatPostMessageResponse response = methods.chatPostMessage(request);
+            if (!response.isOk()) {
+                System.err.println("Slack API Error: " + response.getError());
+                throw new ItCastApplicationException(SLACK_PARSE_ERROR);
+            }
         } catch (Exception e) {
+            System.err.println("Error posting message to Slack: " + e.getMessage());
             throw new ItCastApplicationException(SLACK_PARSE_ERROR);
         }
     }
